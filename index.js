c9fe43a1c91e10bb5b1e10ee8ac19326e8e9cc86
Dananjaya Gokale <dananjaya.g@BRILLIO.COM>
2018-08-20 17:19:19 +0530

    Added Basu Bot

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express= require('express')
 // Load Chance
 var Chance = require('chance');
-
+var axios =require('axios')
 // Instantiate Chance so it can be used
 var chance = new Chance();
 var app=express();
-
+const headers={
+    "Content-type": "application/json",
+    "Authorization" :"Bearer ad201c7c688b4d9f8e7ea205eaf2b030"
+}
 app.use(express.static('public'))
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 let a=0;
-let UserList=[]
+let UserList=[{id:0,name:'Bullet Basya'}]
 function emitToall(data){
   if(io){
       io.emit('userList',data)
@@ -29,21 +32,32 @@ io.on('connection',(socket)=>{
           if(index!=-1){
             UserList.splice(index,1)
           }
-          console.log(UserList)
           emitToall(UserList)
         
     })
     socket.on('send',(datta)=>{
-            console.log(datta)
-            data={
+            let data={
                 msg:datta,
                 usr:user.name
             }
             io.emit("receive",data)
+            if(UserList.length<=2){
+                axios.get(`https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=${datta}&sessionId=12345&timezone=America/New_York`,{headers:headers}).then((res)=>{
+                        console.log(res.data.result.fulfillment.speech);
+                        let data={
+                            msg:res.data.result.fulfillment.speech,
+                            usr:"Bullet Basya (I am UK(Uttar Karnataka) bot)"
+                        }
+                        io.emit('receive',data)
+                }).catch((err)=>{
+                       console.log(err)
+                })
+            }
+           
     })
 
 })
 
-http.listen(process.env.PORT || 8080, function(){
+http.listen(process.env.PORT || 8000, function(){
     console.log('listening on *:3000');
 });
